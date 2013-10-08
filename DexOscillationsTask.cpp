80461d58b432d4a0f7cb4e5c128661c547a6b856
Vincent <vtheate@MacBook-Air-de-admin-2.local>
2013-10-08 15:07:03 +0200

    set tone frequency and tone duration

--- DexOscillationsTask.cpp
+++ DexOscillationsTask.cpp
@@ -31,13 +31,13 @@
 int oscillationUpperTarget = 2;						// Targets showing desired amplitude of cyclic movement.
 int oscillationLowerTarget = 10;
 int oscillationCenterTarget = 6;
-double oscillationDuration = 20.0;
-double oscillationEntrainDuration = 4.0;
-
-double oscillationMaxTrialTime = 120.0;		// Max time to perform the whole list of movements.
-double oscillationMinMovementExtent = 30.0;	// Minimum amplitude along the movement direction (Y). Set to 1000.0 to simulate error.
+double oscillationDuration = 30.0;
+double oscillationEntrainDuration = 10.0; // Audio metronom duration on one trial
+double osc_time_step = 1/1.5; // Oscillation Period
+double oscillationMaxTrialTime = 30.0;		// Max time to perform the whole list of movements.
+double oscillationMinMovementExtent = 19.0;	// Minimum amplitude along the movement direction (Y). Set to 1000.0 to simulate error.
 double oscillationMaxMovementExtent = 1000.0; //HUGE;	// Maximum amplitude along the movement direction (Y). Set to 1000.0 to simulate error.
-int	oscillationMinCycles = 20;	// Minimum cycles along the movement direction (Y). Set to 1000.0 to simulate error.
+int	oscillationMinCycles = 5;	// Minimum cycles along the movement direction (Y). Set to 1000.0 to simulate error.
 int oscillationMaxCycles = 40;	// Maximum cycles along the movement direction (Y). Set to 1000.0 to simulate error.
 double oscillationCycleHysteresis = 10.0;	// Parameter used to adjust the detection of cycles. 
 Vector3	oscillationDirection = {0.0, 1.0, 0.0};	// Oscillations are nominally in the vertical direction. Could change at some point, I suppose.
@@ -60,15 +60,13 @@ int RunOscillations( DexApparatus *apparatus, const char *params ) {
 
 	direction = ParseForDirection( apparatus, params, posture, bar_position, direction_vector, desired_orientation );
 
-	// Tell the subject which configuration should be used.
-	status = apparatus->fWaitSubjectReady( NULL,
-		"Install the DEX Target Frame in the %s Position.\nPlace the Target Bar in the %s position.\nPlace the tapping surfaces in the %s position.\n\nPress <OK> when ready.",
-		PostureString[posture], TargetBarString[bar_position], TappingSurfaceString[TappingUnknown] );
+	// Verify that the apparatus is in the correct configuration, and if not, 
+	//  give instructions to the subject about what to do.
+	apparatus->ShowStatus( "Hardware configuration check ..." );
+	status = apparatus->SelectAndCheckConfiguration( posture, bar_position, DONT_CARE );
 	if ( status == ABORT_EXIT ) exit( status );
 
-	// Verify that it is in the correct configuration, and if not, 
-	//  give instructions to the subject about what to do.
-	status = apparatus->SelectAndCheckConfiguration( posture, bar_position, TappingUnknown );
+	status = apparatus->WaitSubjectReady( "Pictures\\Folded.bmp", "Check that tapping surfaces are folded.\nPress OK when ready to continue." );
 	if ( status == ABORT_EXIT ) exit( status );
 
 		// Instruct subject to take the specified mass.
@@ -78,7 +76,7 @@ int RunOscillations( DexApparatus *apparatus, const char *params ) {
 
 		// Instruct subject to pick up the manipulandum
 	//  and wait for confimation that he or she is ready.
-	status = apparatus->WaitSubjectReady( NULL, "Hold the manipulandum vertically with thumb and \nforefinger centered. \nPress OK when ready to continue." );
+	status = apparatus->WaitSubjectReady( "Pictures\\Manip_in_hand.bmp", "Hold the manipulandum with thumb and \nforefinger centered. \nPress OK when ready to continue." );
 	if ( status == ABORT_EXIT ) exit( status );
    
 	// Check that the grip is properly centered.
@@ -87,7 +85,7 @@ int RunOscillations( DexApparatus *apparatus, const char *params ) {
 
 
 	// Tell the subject which configuration should be used.
-	status = apparatus->fWaitSubjectReady( NULL, "Align the manipulandum with the flashing target \nand then oscillate it between the two lid targets \nfollowing the frequency given by sound. " );
+	status = apparatus->fWaitSubjectReady( "Pictures\\Oscillation.bmp", "Align the manipulandum with the flashing target \nand then oscillate it between the two lid targets \nfollowing the frequency given by sound. " );
 	if ( status == ABORT_EXIT ) exit( status );
 
 	// Light up the central target.
@@ -110,13 +108,15 @@ int RunOscillations( DexApparatus *apparatus, const char *params ) {
 	apparatus->TargetOn( oscillationUpperTarget );
 	
 	// Output a sound pattern to establish the oscillation frequency.
-	// This will play for the first 4 seconds of the trial.
-	double osc_time_step = 0.025;
+	// This will play for the first 10 seconds of the trial.
+	
 	double time;
 	for ( time = 0.0; time < oscillationEntrainDuration; time += osc_time_step ) {
-		int tone = floor( 3.9 * ( sin( time * 2.0 * PI ) + 1.0 ) );
+		int tone = 3.9; //floor( 3.9 * ( sin( time * 2.0 * PI ) + 1.0 ) );
 		apparatus->SetSoundStateInternal( tone, 1 );
-		apparatus->Wait( osc_time_step - 0.01 );
+		apparatus->Wait( osc_time_step/2 - 0.01 );
+		apparatus->SetSoundStateInternal( tone, 0 );
+        apparatus->Wait( osc_time_step/2 - 0.01 );
 	}
 	apparatus->SetSoundState( 4, 0 );
 
