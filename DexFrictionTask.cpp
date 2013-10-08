dcf7bc7d02591dad3a736ce4f0553ec4fd92d10c
Vincent <vtheate@MacBook-Air-de-admin-2.local>
2013-10-08 10:59:46 +0200

    Change Friction text and pictures

--- DexFrictionTask.cpp
+++ DexFrictionTask.cpp
@@ -25,17 +25,17 @@ double frictionHoldTime = 2.0;
 double frictionTimeout = 200.0;
 
 // These are easy, but probably too easy.
-double frictionMinGrip = 5.0;
-double frictionMaxGrip = 20.0;
-double frictionMinLoad = 2.0;
+double frictionMinGrip = 2.0;
+double frictionMaxGrip = 5.0;
+double frictionMinLoad = -20.0;
 double frictionMaxLoad = 20.0;
 double forceFilterConstant = 1.0;
 
 // Define the pull direction. This should be up.
-Vector3 frictionLoadDirection = { 0.0, 1.0, 0.0 };
+Vector3 frictionLoadDirection = { 0.0, 0.0, 1.0 };
 
-double slipThreshold = 10.0;
-double slipTimeout = 200.0;
+double slipThreshold = 100.0;
+double slipTimeout = 10.0;
 
 /*********************************************************************************/
 
@@ -52,16 +52,13 @@ int RunFrictionMeasurement( DexApparatus *apparatus, const char *params ) {
 	//  DEX apparatus, which in theory can poll and sample continuously at the same time.
 	apparatus->adc->AllowPollingDuringAcquisition();
 	
-	status = apparatus->WaitSubjectReady( "Squeeze the manipulandum between thumb and index with the right hand.\nBe sure that the thumb and the forefinger are centered.\n\nPress OK when ready to continue." );
+	status = apparatus->WaitSubjectReady( "Pictures\\Coef_frict.bmp", "Squeeze the manipulandum between thumb and index with the right hand.\nBe sure that the thumb and the forefinger are centered.\n\nPress OK when ready to continue." );
 	if ( status == ABORT_EXIT ) return( status );
 
 	status = apparatus->WaitCenteredGrip( copTolerance, 1.0, 1.0, "Grip not centered." );
 	if ( status == ABORT_EXIT ) exit( status );
 
-	status = apparatus->WaitSubjectReady( "Pull Upward.\nAdjust pinch and pull forces according to LEDs.\n\nPress OK when ready to continue." );
-	if ( status == ABORT_EXIT ) return( status );
-
-    status = apparatus->WaitSubjectReady( "When you will hear the beep, pull harder until slippage.\n\nPress OK when ready to continue." );
+	status = apparatus->WaitSubjectReady( "Pictures\\Coef_frict_osc.bmp","Rub the manipulandum with the \nadjusted pinch force according to LEDs.\n\nPress OK when ready to continue." );
 	if ( status == ABORT_EXIT ) return( status );
 
     apparatus->StartAcquisition( "FRIC", maxTrialDuration );
@@ -74,13 +71,13 @@ int RunFrictionMeasurement( DexApparatus *apparatus, const char *params ) {
 		forceFilterConstant, frictionHoldTime, frictionTimeout, "Desired force not achieved." );
 	apparatus->MarkEvent( FORCE_OK );
 
-	apparatus->Beep();
+	//apparatus->Beep();
     
-	apparatus->WaitSlip( frictionMinGrip, frictionMaxGrip, 
-		frictionMinLoad, frictionMaxLoad, frictionLoadDirection, 
-		forceFilterConstant, slipThreshold, slipTimeout, "Slip not achieved."  );
-	apparatus->MarkEvent( SLIP_OK );
-	apparatus->HideStatus();
+	//apparatus->WaitSlip( frictionMinGrip, frictionMaxGrip, 
+	//	frictionMinLoad, frictionMaxLoad, frictionLoadDirection, 
+	//	forceFilterConstant, slipThreshold, slipTimeout, "Slip not achieved."  );
+	//apparatus->MarkEvent( SLIP_OK );
+	//apparatus->HideStatus();
 
 	apparatus->StopAcquisition();
 
