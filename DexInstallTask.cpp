3f0d6fde8ecd689fcef7a4162b469f93589b097a
frenchjam <joe.mcintyre@parisdescartes.fr>
2014-02-11 16:49:55 +0100

    Generic installation task.
    Guide the operator through the steps of putting the hardware in to the
    right configuration and then performing the tracker alignment. The
    routine parses the command line to see if it should be done in the
    upright or supine configurations and tailors the messages and
    parameters accordingly.

--- DexInstallTask.cpp
+++ DexInstallTask.cpp
@@ -33,9 +33,9 @@
 //  which we expect to see the referencce markers.
 // Remember, these are in the intrinsic reference frame of the CODA unit, where
 //  X is along the length of the bar, Y is the distance from the bar and Z is 'above' the bar.
-double fov_min_x = -1000.0, fov_max_x = 1000.0;
+double fov_min_x = -1500.0, fov_max_x = 1500.0;
 double fov_min_y =  1500.0, fov_max_y = 4000.0;
-double fov_min_z = -1000.0, fov_max_z = 1000.0;
+double fov_min_z = -1500.0, fov_max_z = 1500.0;
 
 double codaUnitOrientationTolerance = 30.0;		// Allowable rotation wrt expected orientation, in degrees.
 double codaUnitPositionTolerance = 500.0;		// Allowable displacement wrt expected position, in mm.
@@ -48,6 +48,25 @@ int		alignmentRequiredGood = 2;				// How many of the markers used to check the 
 												//  or that some are disturbed by a poor viewing angle or a reflection.
 
 double alignmentAcquisitionDuration = 5.0;		// Acquire some data to verify where the chair and frame were at the time of alignment.
+
+// The following are the specifications of the expected position and orientation of the
+//  CODA units, depending on whether the alignment was done in the upright or supine configurations.
+// Values are rounded versions of data taken from the DEX EM in Toulouse.
+
+// Coda Unit #1
+Vector3		expected_coda1_position_upright = { -500, 800, 2400};
+Quaternion	expected_coda1_orientation_upright = {0.5, -0.5, 0.5, -0.5};
+
+Vector3		expected_coda1_position_supine = { 500, 2600, 500};
+Quaternion	expected_coda1_orientation_supine = {-0.707107, 0.0, .707107, 0.0};
+
+// Coda Unit #2
+Vector3		expected_coda2_position_upright = {     0, 1200, 2200};
+Quaternion	expected_coda2_orientation_upright = {0.0, -0.707, 0.707, 0.0};
+
+Vector3		expected_coda2_position_supine = { 0, 2600, 900};
+Quaternion	expected_coda2_orientation_supine = { 1.0, 0.0, 0.0, 0.0};
+
 
 /*********************************************************************************/
 
@@ -65,8 +84,7 @@ unsigned long fovMarkerMask = 0x00000f00;
 
 int RunInstall( DexApparatus *apparatus, const char *params ) {
 
-	int status;
-    Quaternion transition_orientation[3];
+	int status = 0;
 
 	// Check which configuration is to be used and prompt the subject to install the apparatus accordingly.
 	DexSubjectPosture desired_posture = ParseForPosture( params );
@@ -80,89 +98,66 @@ int RunInstall( DexApparatus *apparatus, const char *params ) {
 
 	// Check that the 4 reference markers and the manipulandum are in the ideal field-of-view of each Coda unit.
 	apparatus->ShowStatus( "Checking field of view ..." );
-	status = apparatus->CheckTrackerFieldOfView( 0, fovMarkerMask, fov_min_x, fov_max_x, fov_min_y, fov_max_y, fov_min_z, fov_max_z );
+	status = apparatus->CheckTrackerFieldOfView( 0, fovMarkerMask, fov_min_x, fov_max_x, fov_min_y, fov_max_y, fov_min_z, fov_max_z,
+		"Markers not centered for CODA Unit #1.\n - Is the unit properly boresighted?\n - Are the box and bar markers visible?", "alert.bmp" );
 	if ( status == ABORT_EXIT || status == RETRY_EXIT ) return( status );
+
 	if ( apparatus->nCodas > 1 ) {
-		status = apparatus->CheckTrackerFieldOfView( 1, fovMarkerMask, fov_min_x, fov_max_x, fov_min_y, fov_max_y, fov_min_z, fov_max_z );
+		status = apparatus->CheckTrackerFieldOfView( 1, fovMarkerMask, fov_min_x, fov_max_x, fov_min_y, fov_max_y, fov_min_z, fov_max_z,
+			"Markers not centered for CODA Unit #2.\n - Is the unit properly boresighted?\n - Are the box and bar markers visible?", "alert.bmp" );
 		if ( status == ABORT_EXIT || status == RETRY_EXIT ) return( status );
 	}
 
 	// Perform the alignment based on those markers.
 	apparatus->ShowStatus( "Performing alignment ..." );
-	status = apparatus->PerformTrackerAlignment();
+	status = apparatus->PerformTrackerAlignment( "Error performing the tracker alignment.\n - Is the target mast in the right-hand position?\n - Are the reference markers in view?" );
 	if ( status == ABORT_EXIT || status == RETRY_EXIT ) return( status );
+
 
 	// Are the Coda bars where we think they should be?
 	apparatus->ShowStatus( "Check tracker placement ..." );
-
-	// Where do we expect the CODAs to be with respect to the reference frame?.	
-	Vector3 expected_position;
-	Quaternion expected_orientation;
-
-
-	//	Vector3 expected_position[2] = { {-600.0, 300.0, 2000.0}, {-100.0, 800.0, 2000.0} };
-
 	if ( desired_posture == PostureSeated ) {
 
-		// Coda Unit #1 (referenced as unit 0 here) is located to the right of the subject (- X) in a vertical orientation.
-		expected_position[X] = -600;
-		expected_position[Y] =  300;
-		expected_position[Z] = 2500;
-
-		// Need to make 2 quaternion rotations for the vertical Coda bar.
-		apparatus->SetQuaterniond( transition_orientation[0],  90.0, apparatus->iVector );
-		apparatus->SetQuaterniond( transition_orientation[1],  90.0, apparatus->jVector);
-		apparatus->MultiplyQuaternions(expected_orientation,transition_orientation[0],transition_orientation[1]);
 		status = apparatus->CheckTrackerPlacement( 0, 
-											expected_position, codaUnitPositionTolerance, 
-											expected_orientation, codaUnitOrientationTolerance, 
-											"Placement error - Coda Unit 1.-Is setup in SEATED configuration?-Are coda units arranged properly?", "SetupSeated.bmp" );
+											expected_coda1_position_upright, codaUnitPositionTolerance, 
+											expected_coda1_orientation_upright, codaUnitOrientationTolerance, 
+											"Placement error - Coda Unit 1.\n -Is setup in SEATED configuration?\n -Are coda units arranged properly?", "SetupSeated.bmp" );
 	}
 	else {
-
-		// If the calibration was performed in the supine configuration, Coda Unit #1 is to the left (+X) and distance is Y..
-		expected_position[X] =  300;
-		expected_position[Y] = 2500;
-		expected_position[Z] =  300;
-
-		// Need to make 2 quaternion rotations for the vertical Coda bar.
-		// *** I DON'T KNOW THE ANSWER!!!! ***  Need to do test and correct this.
-		apparatus->SetQuaterniond( transition_orientation[0], 180.0, apparatus->iVector );
-		apparatus->SetQuaterniond( transition_orientation[1],  90.0, apparatus->jVector);
-		apparatus->MultiplyQuaternions(expected_orientation,transition_orientation[0],transition_orientation[1]);
 		status = apparatus->CheckTrackerPlacement( 0, 
-											expected_position, codaUnitPositionTolerance, 
-											expected_orientation, codaUnitOrientationTolerance, 
-											"Placement error - Coda Unit 1.\n- Is setup in SUPINE configuration?\n - Are coda units arranged properly?", "SetupSupine.bmp" );
+											expected_coda1_position_supine, codaUnitPositionTolerance, 
+											expected_coda1_orientation_supine, codaUnitOrientationTolerance, 
+											"Placement error - Coda Unit 1.\n - Is setup in SUPINE configuration?\n - Are coda units arranged properly?", "SetupSupine.bmp" );
 
 	}
 	if ( status == ABORT_EXIT || status == RETRY_EXIT ) return( status );
 
-#if 0
+
 	if ( apparatus->nCodas > 1 ) {
 
-		// This is for the 'horizontal' coda. I don't know if this is right.
+	if ( desired_posture == PostureSeated ) {
 
-		// Flip it 180° in the horizontal plane (i.e. around the Y axis).
-		apparatus->SetQuaterniond( transition_orientation[1],  180.0, apparatus->kVector );
-		// Tilt from pointing up to pointing slightly down.
-		apparatus->SetQuaterniond( transition_orientation[0],   90.0, apparatus->iVector);
-		apparatus->MultiplyQuaternions(expected_orientation[1],transition_orientation[0], transition_orientation[1]);
-	
 		status = apparatus->CheckTrackerPlacement( 1, 
-											expected_position[1], codaUnitPositionTolerance, 
-											expected_orientation[1], codaUnitOrientationTolerance, 
-											"Placement error - Coda Unit 2." );
-		if ( status == ABORT_EXIT || status == RETRY_EXIT ) return( status );
+											expected_coda2_position_upright, codaUnitPositionTolerance, 
+											expected_coda2_orientation_upright, codaUnitOrientationTolerance, 
+											"Placement error - Coda Unit 2.\n -Is setup in SEATED configuration?\n -Are coda units arranged properly?", "SetupSeated.bmp" );
+	}
+	else {
+		status = apparatus->CheckTrackerPlacement( 1, 
+											expected_coda2_position_supine, codaUnitPositionTolerance, 
+											expected_coda2_orientation_supine, codaUnitOrientationTolerance, 
+											"Placement error - Coda Unit 2.\n - Is setup in SUPINE configuration?\n - Are coda units arranged properly?", "SetupSupine.bmp" );
+
+	}
+	if ( status == ABORT_EXIT || status == RETRY_EXIT ) return( status );
 
 		// Check that the trackers are still aligned with each other.
 		apparatus->ShowStatus( "Check tracker alignment ..." );
-		status = apparatus->CheckTrackerAlignment( alignmentMarkerMask, alignmentTolerance, alignmentRequiredGood, "Coda misaligned!" );
+		status = apparatus->CheckTrackerAlignment( alignmentMarkerMask, alignmentTolerance, alignmentRequiredGood, "Coda misalignment detected!\n - Did a CODA unit get bumped?\n - Are the markers in the line-of-sight?" );
 		if ( status == ABORT_EXIT || status == RETRY_EXIT ) return( status );
 
 	}
 	
-#endif
 
 	// Perform a short acquisition to measure where the manipulandum is.
 	apparatus->ShowStatus( "Acquire data ..." );
