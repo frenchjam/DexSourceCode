2e4dac612113c3d9d65271cecf1f8ad21781f07e
frenchjam <joe.mcintyre@parisdescartes.fr>
2014-02-12 23:46:50 +0100

    Test Install method.

--- DexGenerateCommonScripts.bat
+++ DexGenerateCommonScripts.bat
@@ -1,2 +1,4 @@
 REM Scripts that are common to more than one protocol.
-DexSimulatorApp -offsets -compile=_CancelOffsetsScript.dex
+DexSimulatorApp -offsets -compile=CancelOffsetsScript.dex
+DexSimulatorApp -install -upright -compile=InstallUpright.dex
+DexSimulatorApp -install -supine -compile=InstallSupine.dex
