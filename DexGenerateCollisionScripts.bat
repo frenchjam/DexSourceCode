605a92d42117fc9f4b98e6f4d076d72a3e34f317
frenchjam <joe.mcintyre@parisdescartes.fr>
2014-03-14 14:37:20 +0100

    Cleaned up some files.

--- DexGenerateCollisionScripts.bat
+++
@@ -1,5 +0,0 @@
-REM Scripts related to the Collisions protocols.
-DexSimulatorApp -collisions -upright	-prep	-compile=CollUp0.dex
-DexSimulatorApp -collisions -upright			-compile=CollUp1.dex
-DexSimulatorApp -collisions -supine		-prep	-compile=CollSu0.dex
-DexSimulatorApp -collisions -supine				-compile=CollSu1.dex
