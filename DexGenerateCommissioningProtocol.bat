6e47b1dd1d97cf57f1ed517e520b88ec20674539
frenchjam <psyphy@comcast.net>
2018-02-19 11:17:16 +0100

    Correct numbering of scripts for sensor tests.
    Sensor test scripts do not require a subject size leading character. We use X instead. This was don originally just for the first of 6 scripts. Here I correct it for all scripts.

--- DexGenerateCommissioningProtocol.bat
+++ DexGenerateCommissioningProtocol.bat
@@ -124,13 +124,13 @@ set filename=%tag%.dex
 echo CMD_TASK,%task%,%filename%,%task% Sensor Up-Down Slow
 
 set /A "task=task+1"
-set tag=%sz%%task%DSLW
+set tag=X%task%DSLW
 set filename=%tag%.dex
 %COMPILER% -sensors -depth -frequency=%frequency% -duration=%duration% -tag=%tag% -compile=%filename% 
 echo CMD_TASK,%task%,%filename%,%task% Sensor Front-Back Slow
 
 set /A "task=task+1"
-set tag=%sz%%task%SSLW
+set tag=X%task%SSLW
 set filename=%tag%.dex
 %COMPILER% -sensors -sideways -frequency=%frequency% -duration=%duration% -tag=%tag% -compile=%filename% 
 echo CMD_TASK,%task%,%filename%,%task% Sensor Left-Right Slow
@@ -139,19 +139,19 @@ set duration=10.0
 set frequency=1.33
 
 set /A "task=task+1"
-set tag=%sz%%task%VFST
+set tag=X%task%VFST
 set filename=%tag%.dex
 %COMPILER% -sensors -vertical -frequency=%frequency% -duration=%duration% -tag=%tag% -compile=%filename% 
 echo CMD_TASK,%task%,%filename%,%task% Sensor Up-Down Fast
 
 set /A "task=task+1"
-set tag=%sz%%task%DFST
+set tag=X%task%DFST
 set filename=%tag%.dex
 %COMPILER% -sensors -depth -frequency=%frequency% -duration=%duration% -tag=%tag% -compile=%filename% 
 echo CMD_TASK,%task%,%filename%,%task% Sensor Front-Back Fast
 
 set /A "task=task+1"
-set tag=%sz%%task%SFST
+set tag=X%task%SFST
 set filename=%tag%.dex
 %COMPILER% -sensors -sideways -frequency=%frequency% -duration=%duration% -tag=%tag% -compile=%filename% 
 echo CMD_TASK,%task%,%filename%,%task% Sensor Left-Right Fast
